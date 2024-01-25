d39ee74e904e9bfddcaac698b9c4ffbbfbc64ffa
Finn Kostka <finn.kostka@veldotem>
2024-01-25 13:46:07 +0100

    day save

--- Day10/Calculator.py
+++ Day10/Calculator.py
@@ -19,14 +19,26 @@ operations = {
     "*" : multiply,
     "/" : divide
 }
+def calculator():
+    num1 = float(input("Enter the first number:\n"))
+    for symbol in operations:
+        print(symbol)
 
-num1 = int(input("Enter the first number:\n"))
-num2 = int(input("Enter the second number:\n"))
+    should_continue = True
+    snew = True
+    while should_continue:
+        operation_symbol = input("Pick an operation from the menu:\n")
+        num2 = float(input("Enter the second number:\n"))
+        answer = operations[operation_symbol](num1, num2)
+        print(f"{num1} + {num2} = {answer}")
+        num1 = answer
+        scontinue = input("would you like to continue calculating with the previous answer?y/n\n")
+        if scontinue == "n":
+            snew = input("Do you want to start a new calculation?y/n\n")
+            if snew == "y":
+                calculator()
+            else:
+                should_continue = False
+                exit("Thanks for using this calculator!")
 
-for symbol in operations:
-    print(symbol)
-operation_symbol = input("Pick an operation from the menu:\n")
-
-answer = operations[operation_symbol](num1, num2)
-
-print(f"{num1} + {num2} = {answer}")
+calculator()
